343d7663de3067d2bc389260cbb700ed3e9ec71b
Rafael <rft.ferreira@gmail.com>
2023-01-22 13:48:29 -0300

    Feito os metodos e finalizado.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 import java.time.LocalDate;
 
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Conteudo;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 public class Main {
@@ -24,9 +26,41 @@ public class Main {
         mentoria.setDescricao("Descrição mentoria Java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
         System.out.println(mentoria);
+*/
 
+        Bootcamp bootcamp = new Bootcamp();
+
+        bootcamp.setNome("Bootcamp JAva Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devRafa = new Dev();
+        devRafa.setNome("Rafa");
+        devRafa.inscreverBootcamp(bootcamp);
+
+        System.out.println("Conteúdos inscritos Rafa" + devRafa.getConteudosInscritos());
+        devRafa.progredir();
+        System.out.println("Conteúdos inscritos Rafa" + devRafa.getConteudosInscritos());
+        System.out.println("Conteúdos concluídos Rafa" + devRafa.getConteudosConcluidos());
+        System.out.println("XP: "+ devRafa.calcularXp());
+
+        System.out.println("-----------------");
+        
+        Dev devAna = new Dev();
+        devAna.setNome("Ana");
+        devAna.inscreverBootcamp(bootcamp);
+        
+        System.out.println("Conteúdos inscritos Ana" + devAna.getConteudosInscritos());
+        devAna.progredir();
+        devAna.progredir();
+        devAna.progredir();
+        System.out.println("Conteúdos inscritos Ana" + devAna.getConteudosInscritos());
+        System.out.println("Conteúdos concluídos Ana" + devAna.getConteudosConcluidos());
+        System.out.println("XP: "+ devAna.calcularXp());
     }
 }
